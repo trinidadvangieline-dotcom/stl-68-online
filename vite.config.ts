0ff47fa255a7569f4eb5d4048c1217cc082b71c0
trinidadvangieline-dotcom <trinidadvangieline@gmail.com>
2025-12-31 10:25:22 +0800

    vte.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {} // This ensures process.env doesn't crash in the browser
+  }
+});
